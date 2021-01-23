7a0b7f2ae4f559cc3ba1fdb8892fde134dceae4a
nakashima1125 <free.nakashima@gmail.com>
2021-01-23 23:24:14 +0900

    change

--- slackbot/plugins/my_menthon.py
+++ slackbot/plugins/my_menthon.py
@@ -4,7 +4,6 @@ from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
 
-
 @respond_to('hello')
 def mention_func(message):
     message.reply('hogehoge') # メンション
--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1060330893521-1668400016595-AF962g1dfQLy2dnSa7HDU3bG"
+API_TOKEN = "xoxb-1060330893521-1668400016595-TikrwCGWPuWKtStwfMAv3xzT"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "Not"
