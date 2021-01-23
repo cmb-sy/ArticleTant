9aa90f38b65a8c66f92f9d7d8e308b0b5a03e492
Masahito Kumada <5445197440@edu.k.u-tokyo.ac.jp>
2021-01-23 19:32:49 +0900

    ユーザが調べたい分野について呟くと, arxiv上で提出日が最新の論文を１つ出力するように修正

---
+++ search/Readme.md
@@ -0,0 +1,16 @@
+# backendについて
+
+- 実行プログラム
+    - get_paper_info.py: INPUT_TXTにユーザが調べたい分野について呟くと, arxiv上で提出日が最新の論文を１つ出力する. 
+    - mecab-ipadic-neologd/ : 多数のWeb上の言語資源から得た新語を追加することでカスタマイズした MeCab用のシステム辞書.
+    (https://github.com/neologd/mecab-ipadic-neologd/blob/master/README.ja.md)
+
+
+- 環境構築
+    - mecabインストール
+    - for mac: https://qiita.com/paulxll/items/72a2bea9b1d1486ca751, https://qiita.com/taroc/items/b9afd914432da08dafc8
+    - for windows:  https://qiita.com/menon/items/f041b7c46543f38f78f7s
+    - ※　mecab-ipadic-NEologdもインストールするように。（新語に対応）
+
+    - python library insall
+    - pip install -r requirement.txt
--- search/get_paper_info.py
+++ search/get_paper_info.py
@@ -1,8 +1,18 @@
-from datetime import datetime
-import re
-from googletrans import Translator
-from time import sleep
 import arxiv
+from datetime import datetime
+from sklearn.datasets import fetch_20newsgroups
+import MeCab
+import numpy as np
+from time import sleep
+from sklearn.feature_extraction.text import TfidfVectorizer
+from googletrans import Translator
+import re
+
+
+# arxiv.query()の引数設定
+INPUT_TXT = '自然言語処理、つよつよマン欲しいなあ。' # ユーザが調べたい論文についての呟き.
+MAX_RESULTS = 1
+SORT_BY = "submittedDate"
 
 
 def make_input_txt(keywords, prefix, condition):
@@ -36,20 +46,65 @@ def translate_post(idx, result):
     # sleep(5)
 
 
+def get_keyword(input_txt):
+
+    # 単語の分かちわけができていること前提
+    docs = [extract(input_txt)]
+
+    vectorizer = TfidfVectorizer(min_df=0.03)
+    tfidf_X = vectorizer.fit_transform(docs).toarray()
+
+    index = tfidf_X.argsort(axis=1)[:,::-1] # 各文書のTF-IDF値にもとづいて降順ソートされたindexを得る.
+    # print('index:', index)
+    feature_names = np.array(vectorizer.get_feature_names())
+    # print('feature_names:', feature_names)
+    feature_words = feature_names[index]
+    # print('feature_words:', feature_words)
+
+    return feature_words[0]
+ 
+
+def extract(text):
+
+    # Taggerオブジェクトを生成
+    # デフォルトの辞書を読み込む.
+    # tokenizer = MeCab.Tagger("-Ochasen")
+    # 新語に対応した辞書を読み込む
+    tokenizer = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+    tokenizer.parse("")
+
+    words = []
+
+    # 単語の特徴リストを生成
+    node = tokenizer.parseToNode(text)
+
+    while node:
+        # 品詞情報(node.feature)が名詞ならば
+        if node.feature.split(",")[0] == u"名詞":
+            # 単語(node.surface)をwordsに追加
+            words.append(node.surface)
+        node = node.next
+
+    # 半角スペース区切りで文字列を結合
+    text_result = ' '.join(words)
+    return text_result
+
+
 def main():
 
-    keywords = ["情報学", "生物学"]
+    keywords = get_keyword(INPUT_TXT) # ex: keywords = ["情報学", "生物学"]
 
-    input_txt = make_input_txt(keywords, prefix="abs:", condition="AND")
+    query_txt_jp = make_input_txt(keywords, prefix="abs:", condition="OR")
 
-    query_text = get_translated(input_txt, src="ja", dest="en")
-    results = arxiv.query(query = query_text, max_results=5, sort_by="submittedDate")
+    query_txt_en = get_translated(query_txt_jp, src="ja", dest="en")
+
+    results = arxiv.query(query = query_txt_en, max_results=MAX_RESULTS, sort_by=SORT_BY)
 
     for i, result in enumerate(results):
-
         translate_post(i, result)
         
-    print("DONE")
+    print("DONE.")
+
 
 
 if __name__ == "__main__":
