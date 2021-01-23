beff08c51c50de55647a0c563f595b1f5af93713
nakashima1125 <free.nakashima@gmail.com>
2021-01-23 18:15:18 +0900

    make slack bot

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1060330893521-1664977575317-W8Jj3qJHeGyy1vz2YRcO6mxo"
+API_TOKEN = "xoxb-1060330893521-1668400016595-AF962g1dfQLy2dnSa7HDU3bG"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "Not"
