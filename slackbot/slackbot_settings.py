082556958cf950cb453d0624d7abf8d56ca629ba
nakashima1125 <free.nakashima@gmail.com>
2021-01-23 17:05:05 +0900

    slack

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-1060330893521-1664977575317-W8Jj3qJHeGyy1vz2YRcO6mxo"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "おk"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
