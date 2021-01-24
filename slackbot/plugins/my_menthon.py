51fab0dfbf7b4f82131c372e2bfb0ef6d2ddfa5f
nakashima1125 <free.nakashima@gmail.com>
2021-01-24 14:50:24 +0900

    change1

--- slackbot/plugins/my_menthon.py
+++ slackbot/plugins/my_menthon.py
@@ -1,9 +1,29 @@
 # coding: utf-8
-
+import sys
+sys.path.append("../search")
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+from get_paper_info import main
+# from search.get_paper_info import make_input_txt
 
-@respond_to('hello')
+@respond_to('はじめる')
 def mention_func(message):
-    message.reply('hogehoge') # メンション
+    message.reply("論文キーワードを入力してね")
+
+@listen_to('!essay(.*)$')
+def comment_log(message, text):
+    essay = main(text)
+    # text = essay[0].encode('utf-8')
+    message.reply(essay) 
+
+# @respond_to('/essay')
+# def essay(message):
+#     essay = main()
+#     message.reply(essay) 
+
+
+# @respond_to('input_txt')
+# def mention_func(message):
+#     essay = main()
+#     message.reply(essay) 
--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1060330893521-1668400016595-TikrwCGWPuWKtStwfMAv3xzT"
+API_TOKEN = "xoxb-1060330893521-1668400016595-db7kvAfqb2kRJjnWLDxYrps6"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
-DEFAULT_REPLY = "Not"
+DEFAULT_REPLY = "論文に関するキーワードを入力してね"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ['plugins']
