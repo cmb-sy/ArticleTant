f111fde5e43d9410ac0c4c31b65030ded6235339
nakashima1125 <free.nakashima@gmail.com>
2021-01-25 08:30:17 +0900

    change

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-# botアカウントのトークンを指定
-# API_TOKEN = "xoxb-1060330893521-1668400016595-TikrwCGWPuWKtStwfMAv3xzT" # nakashima
-API_TOKEN = "xoxb-1060330893521-1668400016595-vgnOtLNngzPgAbkkOkZaZthc" #kumada
+API_TOKEN =os.envision["KEY"] #kumada
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "Not"
