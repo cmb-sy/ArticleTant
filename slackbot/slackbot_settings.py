c00767f1db42a9aed7f9ef67fc93e07c90a2a9e3
Masahito Kumada <5445197440@edu.k.u-tokyo.ac.jp>
2021-01-24 14:22:37 +0900

    設定ファイルapi修正

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1060330893521-1668400016595-TikrwCGWPuWKtStwfMAv3xzT"
+# API_TOKEN = "xoxb-1060330893521-1668400016595-TikrwCGWPuWKtStwfMAv3xzT" # nakashima
+API_TOKEN = "xoxb-6184316613-1675081357508-j9Oh4xH3NOp8quYG39YRXLhc" #kumada
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "Not"
