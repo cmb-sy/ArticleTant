77ce4ba8de0fa1877bcc49713aaa4a657569e84a
nakashima1125 <free.nakashima@gmail.com>
2021-01-25 08:20:43 +0900

    change

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-
+import os 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1060330893521-1668400016595-mG7qb3BPeE5TPff3gDdfxkjN"
+API_TOKEN =os.environ["Key"] 
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "論文に関するキーワードを入力してね"
